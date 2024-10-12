c3ea0bdebfaedd057e5c8d40f19d60570e7d2906
David <68209858+donglinzhou@users.noreply.github.com>
2024-10-12 19:18:29 +0800

    Update layer.py

--- stage2_sequence_preference_learning/layer.py
+++ stage2_sequence_preference_learning/layer.py
@@ -1,19 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time   : 2020/6/27 16:40
-# @Author : Shanlei Mu
-# @Email  : slmu@ruc.edu.cn
-# @File   : layers.py
-
-# UPDATE:
-# @Time   : 2022/7/16, 2020/8/24 14:58, 2020/9/16, 2020/9/21, 2020/10/9, 2021/05/01
-# @Author : Zhen Tian, Yujie Lu, Xingyu Pan, Zhichao Feng, Hui Wang, Xinyan Fan
-# @Email  : chenyuwuxinn@gmail.com, yujielu1998@gmail.com, panxy@ruc.edu.cn, fzcbupt@gmail.com, hui.wang@ruc.edu.cn, xinyan.fan@ruc.edu.cn
-
-"""
-recbole.model.layers
-#############################
-Common Layers in recommender system
-"""
 
 import copy
 import math
